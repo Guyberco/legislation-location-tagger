fbe5887391c2c3fd5951f4f9b558d116b5bd3a42
Izzy Zemser <zemser@gmail.com>
2021-01-29 12:13:46 +0200

    changed updating occurrences

--- src/lawHandler.py
+++ src/lawHandler.py
@@ -25,59 +25,6 @@ def initializeDataBase(file, dataBase):
             indx += 1
 
 
-
-
-
-
-# def checkForLocKey(db, lines, indx):
-#     """
-#     given an index in the lines, check if the word in lines[indx] is a key in the db, is so increase the counter in the db.
-#     Also if the word appears as a location add the current counter value to the occurrence list in the db.
-#     :param db:  database of words that are locations
-#     :param lines: all the lines of the file (file here is the output of the tagger)
-#     :param indx: index of line we check
-#     :return: index of the next word to check in lines
-#     """
-#     increase_counter_flag = False
-#     line = lines[indx]
-#     columns = line.split()
-#     if len(columns) < 4:
-#         return indx+1
-#     line_num = columns[0]    # index of line (if we have more than one entry for a word then the next lines would have the same index)
-#     word = columns[3]     # the word as it appears in the xml
-#     while len(columns) >= 3 and line_num == columns[0]:
-#         location = db.getValueByKey(word)
-#         # the word appears in the db so increase the counter before exiting the function
-#         if location:
-#             increase_counter_flag = True
-#             # check if this key occurrence is tagged as location if so add the counter value to the occurrence list
-#             if CheckLocationOcc(columns):
-#                 apppendLocationOcc(location, db.getCounterByKey(word))
-#         indx += 1
-#         if not indx < len(lines):
-#             break
-#         line = lines[indx]
-#         columns = line.split()
-#     # if the word is a key in the db, increase the counter for this key in the db
-#     if increase_counter_flag:
-#         db.increaseCounter(word)
-#     return indx
-
-
-
-# def updateOccurances(file, dataBase):
-#     """
-#     go through the file and for each word that is key in the db (location word), increase the counter for that key in the db. determine if the word context is
-#     a location and if so add the current counter value to the "instancesToTag" list
-#     each key entrance is composed of ( counter:int , instancesToTag:int[] )
-#     :param file: contains the NRE output
-#     :param dataBase: initialized database with all the location keys
-#     """
-#     lines = file.readlines()
-#     indx = 0
-#     while indx < len(lines):
-#         indx = checkForLocKey(dataBase, lines, indx)
-
 def getNextNonEmptyLine(lines, indx):
     while indx < len(lines):
         if not len(lines[indx].split()) < 3:
